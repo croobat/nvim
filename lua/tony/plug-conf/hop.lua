e7ac78263390bc7636135f0b69e68adcb988b851
Tony Ramírez <tonyramirez.business@outlook.com>
2022-09-22 20:30:34 -0500

    changed hop from g/ to alt-/

--- lua/tony/plug-conf/hop.lua
+++ lua/tony/plug-conf/hop.lua
@@ -14,6 +14,6 @@ hop.setup {
 -- vim.api.nvim_set_keymap('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
 -- vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>", {})
 -- vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>", {})
-keymap("n", "g/", ":HopPatternMW<cr>", {})
-keymap("n", "g?", ":HopLineStartMW<cr>", {})
+keymap("n", "<A-/>", ":HopPatternMW<cr>", {})
+keymap("n", "<A-?>", ":HopLineStartMW<cr>", {})
 
