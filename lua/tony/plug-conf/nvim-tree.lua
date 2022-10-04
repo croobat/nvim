2f045de161d128b6e7ff802350f7b81a6b77fc4e
Tony Ramírez <tonyramirez.business@outlook.com>
2022-10-04 11:04:33 -0500

    commented nvim-tree I shortcut (overlaps show gitignored files)

--- lua/tony/plug-conf/nvim-tree.lua
+++ lua/tony/plug-conf/nvim-tree.lua
@@ -68,7 +68,7 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
-        { key = "I", cb = tree_cb "full_rename" },
+        -- { key = "I", cb = tree_cb "full_rename" },
         { key = "A", cb = tree_cb "rename" },
         { key = ",", cb = tree_cb "edit" },
       },
