20869bd27402efa356483134bc3b233166bb71e8
Tony Ramírez <tonyramirez.business@outlook.com>
2022-10-16 09:06:57 -0500

    added auto remove trailing spaces

--- lua/tony/autocommands.lua
+++ lua/tony/autocommands.lua
@@ -1,5 +1,5 @@
 -- Remove trailing spaces upon saving
--- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
---   pattern = { "*" },
---   command = [[%s/\s\+$//e]],
--- })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
