0aa88e530d9fb91d1cbcd0051b78a001c50202ad
Tony Ramírez <tonyramirez.business@outlook.com>
2022-12-02 09:52:41 -0600

    updated nvim-tree

--- lua/plugins/config/nvim-tree.lua
+++ lua/plugins/config/nvim-tree.lua
@@ -1,44 +1,91 @@
+nvim_tree = require("nvim-tree")
+nvim_web_devicons = require("nvim-web-devicons")
+nvim_tree_config = require("nvim-tree.config")
+
 -- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+-- Auto close if only buffer
+local tree_cb = nvim_tree_config.nvim_tree_callback
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+            vim.cmd "quit"
+        end
+    end
 })
-require 'nvim-web-devicons'.setup {
- -- your personnal icons can go here (to override)
- -- you can specify color or cterm_color instead of specifying both of them
- -- DevIcon will be appended to `name`
+
+nvim_tree.setup({
+    hijack_cursor = true,
+    update_cwd = true,
+    respect_buf_cwd = true,
+
+    view = {
+        adaptive_size = true,
+        mappings = {
+            list = {
+                { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+                { key = "h", cb = tree_cb "close_node" },
+                { key = "u", action = "dir_up" },
+                { key = "v", cb = tree_cb "vsplit" },
+            },
+        },
+    },
+
+    diagnostics = {
+        enable = true,
+    },
+
+    update_focused_file = {
+        enable = true,
+        update_root = true,
+    },
+
+    git = {
+        enable = true,
+        ignore = false,
+        timeout = 500,
+    },
+
+    actions = {
+        open_file = {
+            quit_on_open = true,
+        },
+    },
+
+    renderer = {
+        highlight_git = true,
+        root_folder_label = ":t",
+        icons = {
+            glyphs = {
+                git = {
+                  unstaged = "",
+                  staged = "S",
+                  untracked = "U",
+                  deleted = "",
+                  ignored = "◌",
+                },
+            },
+            show = {
+                git = true,
+                folder = true,
+                file = true,
+                folder_arrow = true,
+            }
+        },
+    },
+})
+
+nvim_web_devicons.setup {
  override = {
-  zsh = {
-    icon = "",
-    color = "#428850",
-    cterm_color = "65",
-    name = "Zsh"
-  }
- };
- -- globally enable different highlight colors per icon (default to true)
- -- if set to false all icons will have the default icon's color
- color_icons = true;
- -- globally enable default icons (default to false)
- -- will get overriden by `get_icons` option
- default = true;
+    zsh = {
+        icon = "",
+        color = "#428850",
+        cterm_color = "65",
+        name = "Zsh"
+    }
+};
+ default = true; -- globally enable default icons
 }
