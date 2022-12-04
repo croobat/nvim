1b4126956e19616277d57e8a1f4b72a985b129fa
Tony Ramírez <tonyramirez.business@outlook.com>
2022-12-04 16:12:21 -0600

    added custom snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -107,10 +107,6 @@ cmp.setup.cmdline(':', {
     })
 })
 
--- Load snippets
--- Lazy load VSlike (friendly snippets)
-require("luasnip.loaders.from_vscode").lazy_load()
-
 -- Set up lspconfig.
 -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -2,23 +2,28 @@ local luasnip= require "luasnip"
 local types = require "luasnip.util.types"
 
 luasnip.config.set_config {
-  -- Remember last snippet
-  history = true,
+    -- Remember last snippet
+    history = true,
 
-  -- Update dynamic snips in real time
-  updateevents = "TextChanged,TextChangedI",
+    -- Update dynamic snips in real time
+    updateevents = "TextChanged,TextChangedI",
 
-  -- Autosnippets:
-  enable_autosnidsfdsfppets = true,
+    -- Autosnippets:
+    enable_autosnidsfdsfppets = true,
 
-  --Cool highlights
-  -- #vid3
-  -- ext_opts = nil
-  ext_opts = {
-    [types.choiceNode] = {
-      active = {
-        virt_text = { { "<-", "Error" } }
-      },
+    --Cool highlights
+    -- #vid3
+    -- ext_opts = nil
+    ext_opts = {
+        [types.choiceNode] = {
+            active = {
+                virt_text = { { "<-", "Error" } }
+            },
+        },
     },
-  },
 }
+
+-- Load snippets
+-- Lazy load VSlike (friendly snippets)
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
