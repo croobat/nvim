095a7800cb8f28641140f3c221065e84e1bdbd19
Tony Ramírez <tonyramirez.business@outlook.com>
2022-12-04 19:37:23 -0600

    improved hardline visuals

--- lua/plugins/hardline.lua
+++ lua/plugins/hardline.lua
@@ -7,15 +7,15 @@ require('hardline').setup {
   theme = 'tony',   -- change theme
   sections = {         -- define sections
     --{class = 'mode', item = require('hardline.parts.mode').get_item},
-    {class = 'high', item = require('hardline.parts.git').get_item, hide = 70},
+    {class = 'high', item = require('hardline.parts.git').get_item, hide = 90},
     {class = 'med', item = require('hardline.parts.filename').get_item},
     '%<',
     {class = 'med', item = '%='},
     {class = 'error', item = require('hardline.parts.lsp').get_error},
     {class = 'warning', item = require('hardline.parts.lsp').get_warning},
-    {class = 'med', item = require('hardline.parts.whitespace').get_item, hide = 60},
-    {class = 'high', item = require('hardline.parts.filetype').get_item, hide = 60},
-    {class = 'med', item = require('hardline.parts.wordcount').get_item, hide = 70},
+    {class = 'low', item = require('hardline.parts.whitespace').get_item, hide = 90},
+    {class = 'high', item = require('hardline.parts.filetype').get_item, hide = 90},
+    {class = 'med', item = require('hardline.parts.wordcount').get_item, hide = 90},
     {class = 'low', item = require('hardline.parts.line').get_item},
 
   },
