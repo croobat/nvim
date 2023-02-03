90f57c35716bb8aeb1f602bd2914ea3cc3f7373d
Tony Ramírez <tonyramirez.business@outlook.com>
2023-02-02 21:52:38 -0600

    feat(plugins): add tokyonight colorscheme plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,5 +14,11 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 
 require("lazy").setup({
+	-- Colorscheme
+	{ "folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function() require "plugins.tokyonight" end,
+	},
 })
 
