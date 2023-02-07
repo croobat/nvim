9f5673bf71e460eda63f519ca1588229bec5f86f
Tony Ramírez <tonyramirez.business@outlook.com>
2023-02-06 21:20:49 -0600

    fix(telescope): remove redundant mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -91,6 +91,3 @@ telescope.setup {
 
 --telescope.load_extension('media_files')
 telescope.load_extension('fzf')
-
-vim.api.nvim_set_keymap("n", "<leader>/", ":Telescope live_grep<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>?", ":Telescope find_files<cr>", { noremap = true, silent = true })
