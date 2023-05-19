4f55472189bbc3202c31632e03064df2d23f7e79
Tony Ramírez <tonyramirez.business@outlook.com>
2023-05-19 10:16:42 -0600

    handle --noplugin option

--- init.lua
+++ init.lua
@@ -1,8 +1,20 @@
-require "impatient"
+-- Check if the 'noplugin'
+local noplugin = false
+for _, arg in ipairs(vim.v.argv) do
+	if arg == '--noplugin' then
+		noplugin = true
+		break
+	end
+end
 
 require "core.options"
 require "core.keymaps"
 require "core.autocommands"
 
-require "plugins"
-require "lsp"
+if not noplugin then
+	require "impatient"
+	require "plugins"
+	require "lsp"
+else
+	vim.cmd("colorscheme tokyonight-night")
+end
